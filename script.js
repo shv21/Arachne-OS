5d1966e3aa7be4f73d374405a3736904edc143f4
shivrajjadhav77 <shivrajjadhav2123@gmail.com>
2026-08-03 20:19:23 +0530

    add a toggle funtionality on the start button for opening app menu

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const lockScreen = document.querySelector(".descktop-lockScreen");
 const mainScreen = document.querySelector(".descktop-main-screen");
 const btn = document.querySelector(".login-btn");
+const startBtn = document.querySelector(".start");
+const appBOx = document.querySelector(".appBox");
 
 btn.addEventListener("click", () => {
     mainScreen.classList.remove("hide");
@@ -8,4 +10,7 @@ btn.addEventListener("click", () => {
     console.log("clicked");
 });
 
+startBtn.addEventListener("click", () => {
+    appBOx.classList.toggle("hide");
+});
 
